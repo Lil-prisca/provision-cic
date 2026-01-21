199f6abfcc5be362ba03a49b66935fdda215e501
Oluwadarasimi Adeyinka <adeyinkaoluwadarasimi@gmail.com>
2026-01-21 10:37:20 +0100

    eror linkingg Whereweoperate

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,7 @@ import SideBar from "../../components/SideBar";
 import WelcometoProvision from "../../pages/Home/welcometoProvision";
 import Whatarewe from "../../pages/Home/Whatarewe";
 import Whatwedo from "../../pages/Home/Whatwedo";
-import Whereweoperate from "./whereweoperate";
+import Whereweoperate from "../../pages/Home/Whereweoperate";
 import Footer from "../../components/Footer";
 const Home = () => {
   return (
