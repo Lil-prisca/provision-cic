59348f15d1571e24d4c0e42d57abfe70881003dd
Oluwadarasimi Adeyinka <adeyinkaoluwadarasimi@gmail.com>
2026-01-21 14:01:54 +0100

    used useeffect logic and current window width to show navbar and sidebar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, UseEffect } from "react";
+
 import Header from "../../components/Header";
 import SideBar from "../../components/SideBar";
 import WelcometoProvision from "../../pages/Home/welcometoProvision";
@@ -8,11 +9,25 @@ import Whereweoperate from "../../pages/Home/Whereweoperate";
 import Footer from "../../components/Footer";
 import WhyChooseUs from "../../pages/Home/WhyChooseUs";
 import LatestEvents from "../../pages/Home/LatestEvent";
+import { useEffect } from "react";
+
 const Home = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+  const [isMobile, setIsMobile] = useState(width < 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+      setIsMobile(window.innerWidth < 768);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
   return (
     <div>
       {/* <Header /> */}
-      <SideBar />
+      {/* <SideBar /> */}
+      {isMobile ? <SideBar /> : <Header />}
       <WelcometoProvision />
       <Whatarewe />
       <Whatwedo />
